40a68940c592cd2330f5702a8a1c5eb2757d774b
Morten V. Pedersen <morten@mortenvp.com>
2019-04-07 23:39:45 +0200

    working on it

--- test/test_link_mapper.py
+++ test/test_link_mapper.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 
 def transform_keys(data, key, function):
@@ -22,9 +23,50 @@ def transform_keys(data, key, function):
             transform_keys(v, key, function)
 
 
+# Keywords used to split a C++ type into it's basic elements:
+#
+#
+keywords = ["<", ">", ")", "(", "&", "*", ",", "const", "constexpr"]
+keyword_pattern = '(' + '|'.join([re.escape(k) for k in keywords]) + ')'
+whitespace_pattern = "(^[ \t]|[ \t]$)"
+
+
+def split_cpptype(cpptype):
+    """ Split a C++ type into it's basic string components.
+
+    Example:
+
+        Given: "const std::function<void(uint32_t*, long double)>&"
+
+        We want a list consiting of the following strings:
+
+        result = ["const", " ", "std::function", "<", "void", "(",
+                    "uint32_t", ",", " ", "long double", ")", ">", "&"]
+
+        This will allow us to match the basic types to generate links
+
+
+    By joining the strings the original type should be returned.
+
+    :param cpptype: The C++ type to split
+    """
+
+    # Split based on keywords
+    items = [res for res in re.split(keyword_pattern, cpptype) if res]
+
+    # Build the final result
+    result = []
+
+    # Make sure we preseve white space
+    for item in items:
+        result += [res for res in re.split(whitespace_pattern, item) if res]
+
+    return result
+
 # Basically we want to create links to both internal and external docs
 # for the internal docs we may see a type but we need to know the scope where
 # the type was seen to look in our i
+
 
 class LinkMapper(object):
 
@@ -56,3 +98,23 @@ def test_key():
                 'sdf': {"type": {"value": 3}}}
 
     assert d == expected
+
+
+def test_split_cpptype():
+
+    cpptype = "const std::function<void(uint32_t*, long double)>&"
+
+    result = split_cpptype(cpptype)
+
+    expected = ["const", " ", "std::function", "<", "void", "(", "uint32_t", "*"
+                ",", " ", "long double", ")", ">", "&"]
+
+    assert result == expected
+
+    cpptype = "std::vector<unsigned long long int>&&"
+
+    result = split_cpptype(cpptype)
+
+    expected = ["std::vector", "<", "unsigned long long int", ">", "&", "&"]
+
+    assert result == expected
